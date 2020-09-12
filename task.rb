89617547b3ce943461a4108bfd1e9f8d6e5ebd88
Kenichi Shimada <tobeshimachan@yahoo.co.jp>
2020-09-12 18:33:24 +0900

    Ruby Q6-Q10

--- task.rb
+++ task.rb
@@ -47,14 +47,15 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-
+  numbers2 = numbers1.map{ |i| i *10 }
+  p numbers2
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
-
+  array = array.map(&:to_i)
   # 以下は変更しないで下さい
   p array
 end
@@ -63,6 +64,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
+  programming_languages = programming_languages.map(&:capitalize)
+  upper_case_programming_languages = programming_languages.map(&:upcase)
 
   # 以下は変更しないで下さい
   p programming_languages
@@ -73,14 +76,22 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  for i in 0..3 do
+    puts "会員No.#{i+1} #{names[i]}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  for i in 0..6 do
+    if foods[i] == 'うに'
+        puts "好物です"
+    else
+        puts "まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
